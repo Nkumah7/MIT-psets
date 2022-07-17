3df3ba9e44d9e4061eb515b3f925b8ab28755538
JoTeqDojo <joteqdojo@gmail.com>
2022-07-17 12:50:13 +0100

    modified word game

--- PS3/ps3.py
+++ PS3/ps3.py
@@ -1,12 +1,3 @@
-# 6.0001 Problem Set 3
-#
-# The 6.0001 Word Game
-# Created by: Kevin Luu <luuk> and Jenna Wiens <jwiens>
-#
-# Name          : <your name>
-# Collaborators : <your collaborators>
-# Time spent    : <total time>
-
 import math
 import random
 import string
